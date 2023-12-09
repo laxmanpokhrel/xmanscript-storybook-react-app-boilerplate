6ee3413c5857639d1781f7b6bf4d683ace6ad905
laxmanpokhrel <laxmanpokhrel102@gmail.com>
2023-12-09 14:19:37 +0545

    Initial commit

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+const { mergeConfig } = require("vite");
+// use `mergeConfig` to recursively merge Vite options
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      // Add storybook-specific dependencies to pre-optimization
+      optimizeDeps: {
+        include: ["storybook-addon-designs"],
+      },
+    });
+  },
+};
+export default config;
